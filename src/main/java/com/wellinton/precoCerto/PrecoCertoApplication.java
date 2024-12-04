9362ee0d1f04970802590a38ad44e53b62f39fa4
Wellinton <wellintonhf123@gmail.com>
2024-12-04 09:16:48 -0300

    Primeira sincronização

---
+++ src/main/java/com/wellinton/precoCerto/PrecoCertoApplication.java
@@ -0,0 +1,13 @@
+package com.wellinton.precoCerto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecoCertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecoCertoApplication.class, args);
+	}
+
+}
