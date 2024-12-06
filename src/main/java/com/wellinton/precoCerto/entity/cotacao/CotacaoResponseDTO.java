fdd82310bc4f2d5c746d3537a89466fb2193eb6c
Wellinton <wellintonhf123@gmail.com>
2024-12-06 18:00:56 -0300

    Crud indicadores funcional, cotação já está funcionando a listagem

--- src/main/java/com/wellinton/precoCerto/entity/cotacao/CotacaoResponseDTO.java
+++ src/main/java/com/wellinton/precoCerto/entity/cotacao/CotacaoResponseDTO.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Record.java to edit this template
  */
 package com.wellinton.precoCerto.entity.cotacao;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.wellinton.precoCerto.entity.indicador.IndicadorResponseDTO;
 
 import java.time.LocalDateTime;
 
@@ -10,14 +12,31 @@ import java.time.LocalDateTime;
  *
  * @author welli
  */
-public record CotacaoResponseDTO(String description, LocalDateTime dataHora, Double valor) {
-
+public record CotacaoResponseDTO(Long id, IndicadorResponseDTO indicadorResponseDTO, LocalDateTime dataHora, Double valor) {
     
-    public CotacaoResponseDTO(Cotacao cotacao) {
-        this(cotacao.getIndicador().getDescription(),
-                cotacao.getDataHora(),
-                cotacao.getValor()
+     public CotacaoResponseDTO(Cotacao cotacao) {
+        this(
+            cotacao.getId(),
+            new IndicadorResponseDTO(cotacao.getIndicador()),
+            cotacao.getDataHora(),
+            cotacao.getValor()
         );
-        
     }
+     
+     public Long getId() {
+         return id;
+     }
+     
+     public IndicadorResponseDTO getIndicadorResponseDTO() {
+         return indicadorResponseDTO;
+     }
+     
+     @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
+     public LocalDateTime getDataHora() {
+         return dataHora;
+     }
+     
+     public Double getValor() {
+         return valor;
+     }
 }
